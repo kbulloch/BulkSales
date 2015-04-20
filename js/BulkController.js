d5c8abe60ee1cc6e13006c855e8cc3e7f8900315
Kyle Bulloch & Brett Moore <student@epicodus.com>
2015-04-20 14:56:16 -0700

    Add show details button, add to cart button

--- js/BulkController.js
+++ js/BulkController.js
@@ -4,4 +4,8 @@ submarineList.controller('SubmarinesCtrl', function SubmarinesCtrl($scope) {
      { name: "The Extinguisher", depth: 4800, power: "Diesel", price: 900000 },
      { name: "Mariana Express", depth: 35000, power: "Hyrdolisis", price: 4500000 }
    ];
+   $scope.cart = [];
+   $scope.addToCart = function(submarine) {
+     $scope.cart.push(submarine);
+   }
 });

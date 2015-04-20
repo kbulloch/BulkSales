4818968370a3461e65b55dc33f824a3cf7b14e23
Kyle Bulloch & Brett Moore <student@epicodus.com>
2015-04-20 15:18:03 -0700

    add total functionality to shopping cart list

--- js/BulkController.js
+++ js/BulkController.js
@@ -5,7 +5,14 @@ submarineList.controller('SubmarinesCtrl', function SubmarinesCtrl($scope) {
      { name: "Mariana Express", depth: 35000, power: "Hyrdolisis", price: 4500000 }
    ];
    $scope.cart = [];
+   $scope.total = 0;
    $scope.addToCart = function(submarine) {
      $scope.cart.push(submarine);
-   }
+     var subtotal = 0;
+     for (var i = 0; i < $scope.cart.length; i++) {
+       var price = $scope.cart[i].price;
+       subtotal += price;
+     }
+     $scope.total = subtotal;
+   };
 });
